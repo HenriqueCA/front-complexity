394dc2516912ead7ece151a4e358f3910e452f42
Flavio <flavio.farias@ccc.ufcg.edu.br>
2019-10-19 10:58:58 -0300

    react router and screens library added

---
+++ src/library/stdScreens.js
@@ -0,0 +1,13 @@
+/*
+This is an implementation file for a preinported screens header
+*/
+
+// main pages
+import App from '../App';
+
+// authentication pages
+import Forgot from '../screens/forgot_password/Forgot';
+
+
+
+export default { App, Forgot }; 
