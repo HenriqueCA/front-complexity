76e6e02719be4297c29756bd9e0504bbfa1ddd63
HenriqueCA <henrique.arriel@ccc.ufcg.edu.br>
2019-10-26 20:54:23 -0300

    doc and fix on request methods

--- src/library/routes/userRoutes.js
+++ src/library/routes/userRoutes.js
@@ -1,66 +1,123 @@
-import {baseURL} from './backendRequest';
+import { baseURL } from './backendRequest';
 import axios from 'axios';
 import TOKEN from '../token';
 
+
+const headers = {
+    'Authorization': localStorage.getItem(TOKEN),
+};
+
 const userRoutes = {
+    /**
+     * Rotas relacionadas ao usuário.
+     */
     userRoute: baseURL + '/player',
     loginRoute: this.userRoute + '/login',
     signupRoute: this.userRoute + '/signup',
     forgetPasswordRoute: this.userRoute + '/forgetpassword',
     myProfileRoute: this.userRoute + '/me',
-    logoutRoute: this.profileRoute + '/logout',
-    logoutAllRoute: this.profileRoute + '/logoutall',
-    imageUploadRoute: this.profileRoute + '/image',
-    changePasswordRoute: this.profileRoute + '/password',
+    logoutRoute: this.myProfileRoute + '/logout',
+    logoutAllRoute: this.myProfileRoute + '/logoutall',
+    imageUploadRoute: this.myProfileRoute + '/image',
+    changePasswordRoute: this.myProfileRoute + '/password',
     friendRoute: this.userRoute + '/friend',
     searchPlayer: this.userRoute + '/search',
 
-    headers = {
-        Authorization,
-    }
-
-
-    login = async (user) => {
+    /**
+     * Rota para o login de um usuário.
+     * @param {JSON} user JSON com atributo player, que contém um json com email ou nick, e senha.
+     * @returns {JSON} JSON com token.
+     */
+    async login(user) {
         const response = await axios.post(this.loginRoute, user);
         return response;
     },
 
-    signup = async (user) => {
+    /**
+     * Rota para cadastro de um usuário.
+     * @param {JSON} user JSON com player, que contém um json com email, nick e senha.
+     * @returns {JSON} JSON com player e token. 
+     */
+    async signup(user) {
         const response = await axios.post(this.signupRoute, user);
         return response;
     },
 
-    forgetPassword = async (email) => {
-        const response = await axios.post(this.forgetPasswordRoute, {email});
+    /**
+     * Rota para requisitar uma nova senha, caso tenha sido esquecida.
+     * @param {JSON} user JSON com player, que contém um json com email.
+     * @returns {JSON} com message.
+     */
+    async forgetPassword(user) {
+        const response = await axios.post(this.forgetPasswordRoute,user);
         return response;
     },
 
-    getMyProfile = async () =>{
-        localStorage.getItem(TOKEN);
-
+    /**
+     * Rota para adquirir o perfil do usuário que está logado.
+     * @returns {JSON} conténdo o perfil do usuário.
+     */
+    async getMyProfile() {
+        const response = await axios.get(this.myProfileRoute, headers);
+        return response;
     },
 
-    updateMyProfile = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * 
+     * @param {JSON} updates JSON com updates, que é um JSON com name, age, nationality e institution (opcionais). 
+     * @returns {JSON} JSON com message
+     */
+    async updateMyProfile(updates) {
+        const response = await axios.post(this.myProfileRoute, updates, headers);
+        return response;
     },
 
-    logout = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * Remove o token de um usuário.
+     * @returns {JSON}
+     */
+    async logout() {
+        const response = await axios.post(this.logoutRoute, headers);
+        return response;
+    },
+
+    /**
+     * TODO
+     */
+    async logoutAll() {
+        const response = await axios.post(this.logoutAllRoute, headers);
+        return response;
     },
     
-    logoutAll = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * Faz upload de uma imagem para o perfil do usuário.
+     * @param {FormData} image FormData com imagem a ser enviada.
+     */
+    async uploadImage(image) {
+        const h = {
+            'Authorization': headers.Authorization,
+            'content-type': 'multipart/form-data'
+        };
+        const response = await axios.post(this.imageUploadRoute, image, h);
+        return response;
     },
 
-    uploadImage = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * Rota para troca de uma senha do usuário.
+     * @param {JSON} updates JSON com current_password e new_password.
+     * @returns {JSON} com message caso tenha sucesso ou error.
+     */
+    async changePassword(updates) {
+        const response = await axios.post(this.changePasswordRoute, updates, headers);
+        return response;
     },
 
-    changePassword = async () => {
-        localStorage.getItem(TOKEN);
-    },
-
-    getProfile = async (nick) => {
+    /**
+     * Rota para adquirir o perfil de um usuário através de seu nick.
+     * @param {string} nick Nickname do usuário a adquirir o perfil.
+     * @returns {JSON} conténdo o perfil do usuário caso seja achado ou um error.
+     */
+    async getProfile(nick) {
         const response = await axios.get(this.userRoute + '/', {
             params: {
                 nick
@@ -69,15 +126,32 @@ const userRoutes = {
         return response;
     },
 
-    addFriend = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * Rota para adicionar um usuário aos amigos através de seu nick.
+     * @param {JSON} friend JSON com friend, que é o nick do amigo a ser adicionado.
+     * @returns {JSON}
+     */
+    async addFriend(friend) {
+        const response = await axios.post(this.friendRoute, friend, headers);
+        return response;
     },
 
-    deleteFriend = async () => {
-        localStorage.getItem(TOKEN);
+    /**
+     * Rota para deletar um usuário dos amigos através de seu nick.
+     * @param {JSON} friend JSON com friend, que é o nick do amigo a ser deletado.
+     * @returns {JSON}
+     */
+    async deleteFriend(friend) {
+        const response = await axios.delete(this.friendRoute, friend, headers);
+        return response;
     },
 
-    searchPlayer = async (substring) => {
+    /**
+     * Rota para procurar um player através do nick.
+     * @param {string} substring parte do nick a ser procurado.
+     * @returns {JSON} JSON conténdo message (quantidade de resultados encontrados) e results.
+     */
+    async searchPlayer(substring) {
         const response = await axios.get(this.searchPlayer + '/', {
             params: {
                 substring
