d6ef4c60555ca1946ca182a4eb27bacfcda23ac3
AthilaMatheusBorges <athila.borges@ccc.ufcg.edu.br>
2019-11-17 01:16:52 -0300

    clean

--- src/library/stdScreens.js
+++ src/library/stdScreens.js
@@ -14,6 +14,7 @@ import Signup from '../screens/signup/Signup';
 
 import Question from '../screens/question/Question';
 
+import Questions from '../screens/questions/Questions';
 
 
-export default { Home, Forgot, Login, Signup, Profile, Question }; 
+export default { Home, Forgot, Login, Signup, Profile, Question, Questions }; 
